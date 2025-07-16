e11c33a4ca68bc27c130f520cbbd56559d5e7a80
Inva <clement.redoux@gmail.com>
2025-07-16 07:06:46 +0200

    encoder map for two encoders

--- Macropadv3/keymaps/default/keymap.c
+++ Macropadv3/keymaps/default/keymap.c
@@ -54,9 +54,9 @@ bool oled_task_user() {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD)},
-    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU)},
-    [2] = { ENCODER_CCW_CW(UG_VALD, UG_VALU)},
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [2] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
 };
 #endif
 
