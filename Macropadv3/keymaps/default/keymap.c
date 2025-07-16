6a1b1146053196c452a462c43500beda30338866
Inva <clement.redoux@gmail.com>
2025-07-16 06:58:17 +0200

    oled fail

--- Macropadv3/keymaps/default/keymap.c
+++ Macropadv3/keymaps/default/keymap.c
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [0] = LAYOUT_ortho_4x4(
-        KC_NO,   KC_F2,   KC_F3,   KC_NO,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,
         KC_F5,   KC_F6,   KC_F7,   KC_F8,
         KC_F9,   KC_F10,   KC_F11,   KC_F12,
-        KC_NO,   KC_F13,   QK_BOOT,  KC_NO
+        KC_NO,   KC_F13,   KC_BOOT,  KC_NO
     ),
     [1] = LAYOUT_ortho_4x4(
         KC_NO, KC_TRNS, KC_TRNS, KC_NO,
@@ -38,6 +38,19 @@ enum layer_names {
     _GAME
 };
 
+#ifdef OLED_ENABLE
+// Draw to OLED
+bool oled_task_user() {
+    // Set cursor position
+    oled_set_cursor(0, 1);
+    
+    // Write text to OLED
+    oled_write("Hello World!", false);
+    
+    return false;
+}
+
+#endif
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -47,15 +60,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
-#ifdef OLED_ENABLE
-bool oled_task_user () {
-    //set cursor position
-    oled_set_cursor(0, 1);
-    // Write text
-    oled_write("Hello Clement",false);
 
-    return false;
-}
-
-#endif
 
